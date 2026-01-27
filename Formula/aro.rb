f2504579ed5ab59f70f25c800562d69bb8d1ef7f
Kris Simon <kris@ausdertechnik.de>
2026-01-27 22:52:33 +0100

    Update aro to 0.1.0-beta.2

--- Formula/aro.rb
+++ Formula/aro.rb
@@ -1,9 +1,9 @@
 class Aro < Formula
   desc "ARO programming language - Natural language DSL for business logic"
   homepage "https://github.com/arolang/aro"
-  url "https://github.com/arolang/aro/releases/download/0.1.0-beta.1/aro-macos-arm64.tar.gz"
-  sha256 "6468d50b4b08e5eacb7c01ade9af23f9822003ab5c0f16a2f327d147ba74711b"
-  version "0.1.0-beta.1"
+  url "https://github.com/arolang/aro/releases/download/0.1.0-beta.2/aro-macos-arm64.tar.gz"
+  sha256 "55fe11fbadfe69c06d9c4c141f5e554b28de95e85db438f4c2cca2e9a11c6f55"
+  version "0.1.0-beta.2"
   license "MIT"
 
   # Only support macOS for now (ARM64 binary)
